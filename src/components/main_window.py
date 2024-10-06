fc2f3c491a450fba14668b9ef01336d68256179a
kien <trungkiennk4869@gmail.com>
2024-10-06 12:41:49 +0700

    feat: ✨ main qt window with navigation bar, bind dump event

---
+++ src/components/main_window.py
@@ -0,0 +1,24 @@
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from designer.main_window import Ui_MainWindow
+import sys
+
+
+class AppWindow(QMainWindow):
+    def __init__(self, parent=None):
+        QMainWindow.__init__(self)
+        self.ui = Ui_MainWindow()
+
+        self.ui.setupUi(self)
+
+        # Stack window
+        # TODO: Use constant for index value
+        self.ui.customerButton.clicked.connect(lambda: self.ui.stackedWidget.setCurrentIndex(0))
+        self.ui.reservationButton.clicked.connect(lambda: self.ui.stackedWidget.setCurrentIndex(1))
+        self.ui.roomButton.clicked.connect(lambda: self.ui.stackedWidget.setCurrentIndex(2))
+
+
+
+if __name__ == '__main__':
+    app =  QApplication(sys.argv)
+    window = AppWindow()
+    sys.exit(app.exec_())
--- src/main.py
+++ src/main.py
@@ -1,47 +1,29 @@
 import sys
 
-from PyQt5 import QtWidgets
-from PyQt5.QtCore import QAbstractEventDispatcher, QSettings
-from PyQt5.QtGui import QIcon
-from PyQt5.QtWidgets import QApplication, QPushButton
-from sqlalchemy import Column, ForeignKey, Integer, String, except_, text
-from sqlalchemy import create_engine
-from sqlalchemy.orm import Relationship, base, declarative_base
-from sqlalchemy.orm import scoped_session, sessionmaker
+from sqlalchemy import  create_engine
 
-from components.MainWindow import MainWindow
-
+from components.app import App
 
 # ============ Entity table must import here ==============
 from database.models import *
+from database.orm import  bootstrap
+
+# from database.models.user import User
+from utils.settings import DATABASE_SQLITE_FILE
 
 # ========================================================
 
-from database.orm import Session, bootstrap
-# from database.models.user import User
-from utils.constants import APP_NAME
-from utils.settings import DATABASE_SQLITE_FILE
-
-
-
-
 
 def main():
-
     engine = create_engine(f"sqlite:///{DATABASE_SQLITE_FILE}", echo=True)
     # Require to import all class inheritance with Base class (declarative_base)
     # If not explicit engine will not create table for those class
     bootstrap(engine)
 
-    app = QApplication(sys.argv)
-    app.setApplicationName(APP_NAME)
 
-    window = MainWindow()
-    window.setGeometry(10, 100, 200, 500)
+    app = App(sys.argv)
+    app.run()
 
-    window.maximumSize()
-    window.show()
-    app.exec_()
 
 
 if __name__ == "__main__":
