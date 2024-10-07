0cc9c8c03f5f25aceeac98491f2d4a2caa87d001
douqin <dagxuanlam@gmail.com>
2024-10-07 16:45:27 +0700

    fix: main-ui, resctruct folder
    change main_window.py base on my design
    resctruct ui folder: add src base for scene
    
    BREAKING_CHANGE: change ui totally

--- src/components/app_window.py
+++ src/components/app_window.py
@@ -1,25 +1,31 @@
 from PyQt5.QtWidgets import QApplication, QMainWindow
-from designer.main_window import Ui_MainWindow
+from ui.main.main_window import Ui_MainWindow
 import sys
+
+from ui.scene.customer.customer_scene import CustomerScene
+from ui.scene.home.home_scene import HomeScene
+from ui.scene.reservation.reservation_scene import ReservationScene
+from ui.scene.room.room_scene import RoomScene
+from ui.scene.service.service_scene import ServiceScene
+from ui.scene.setting.setting_scene import SettingScene
 
 
 class AppWindow(QMainWindow):
     def __init__(self, parent=None):
         QMainWindow.__init__(self)
         self.ui = Ui_MainWindow()
-
         self.ui.setupUi(self)
-        self.setCentralWidget(self.ui.qwidgetContainer)
-
-        # Stack window
-        # TODO: Use constant for index value
-        self.ui.customerButton.clicked.connect(lambda: self.ui.stackedWidget.setCurrentIndex(0))
-        self.ui.reservationButton.clicked.connect(lambda: self.ui.stackedWidget.setCurrentIndex(1))
-        self.ui.roomButton.clicked.connect(lambda: self.ui.stackedWidget.setCurrentIndex(2))
-
+        self.ui.add_scene([
+            HomeScene(),
+            CustomerScene(),
+            ReservationScene(),
+            RoomScene(),
+            ServiceScene(),
+            SettingScene()
+        ])
 
 
 if __name__ == '__main__':
-    app =  QApplication(sys.argv)
+    app = QApplication(sys.argv)
     window = AppWindow()
     sys.exit(app.exec_())
