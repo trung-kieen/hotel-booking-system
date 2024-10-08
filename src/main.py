317b3631664335b526548c98e45f80717d9c8f46
kien <trungkiennk4869@gmail.com>
2024-10-08 15:11:59 +0700

    Revert "refactor: 🎨 facker data module"
    This reverts commit ff05bad048566de7f0d4d4cb94f0afba04b36ed9.

--- src/main.py
+++ src/main.py
@@ -7,7 +7,7 @@ from components.app import App
 # ============ Entity table must import here ==============
 from database.models import *
 from database.orm import  bootstrap
-from database.fake_data import fake_data
+from fake_data import fake_data
 
 # from database.models.user import User
 from utils.settings import DATABASE_SQLITE_FILE
@@ -22,8 +22,7 @@ def main():
     # If not explicit engine will not create table for those class
     bootstrap(engine)
 
-    fake_data(engine)
-
+    fake_data.fake(engine)
     app = App(sys.argv)
     app.run()
 
