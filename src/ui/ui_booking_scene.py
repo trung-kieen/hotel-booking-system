4edb3e4284ffc9072e41b48849f12e372ddc884b
douqin <dagxuanlam@gmail.com>
2024-10-16 22:15:13 +0700

    fix: rename reservation
    rename reservation (package) in project to booking in order to consistent with Models(Entities)

--- src/ui/ui_booking_scene.py
+++ src/ui/ui_booking_scene.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'ui/ui_reservation_scene.ui'
+# Form implementation generated from reading ui file 'ui/ui_booking_scene.ui'
 #
 # Created by: PyQt5 UI code generator 5.15.11
 #
